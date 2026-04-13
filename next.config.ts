a839566691ac5f27e7a9dab06ead11725856f716
Miguel Alejandre <malejandre@MacBook-Air-de-Miguel.local>
2026-04-13 12:49:49 -0600

    fix: deploy 'output standalone '

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
   
   // Optimizaciones para producción
   compress: true,
